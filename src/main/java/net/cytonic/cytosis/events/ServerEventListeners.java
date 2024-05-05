680abff65de13b6d03cf78b4e687adaa8bc27cdb
foxikle <tristantroop260@gmail.com>
2024-05-05 12:57:43 -0400

    Reformat listeners to use new system with priorities

--- src/main/java/net/cytonic/cytosis/events/ServerEventListeners.java
+++ src/main/java/net/cytonic/cytosis/events/ServerEventListeners.java
@@ -13,20 +13,18 @@ public class ServerEventListeners {
     public static void initServerEvents() {
 
         Logger.info("Registering player configuration event.");
-        Cytosis.getEventHandler().registerGlobalEvent(AsyncPlayerConfigurationEvent.class, event -> {
+        Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-configuration", true, 1, AsyncPlayerConfigurationEvent.class, (event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(Cytosis.getDefaultInstance());
             player.setRespawnPoint(new Pos(0, 45, 0));
-        });
+        })));
 
         Logger.info("Registering player spawn event.");
-        Cytosis.getEventHandler().registerGlobalEvent(PlayerSpawnEvent.class, event -> {
+        Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-spawn", false, 1, PlayerSpawnEvent.class, (event -> {
             final Player player = event.getPlayer();
             if (CytosisSettings.LOG_PLAYER_IPS)
-                Logger.info(event.getPlayer().getUsername() + " (" + event.getPlayer().getUuid() + ") joined with the ip: " + player.getPlayerConnection().getServerAddress());
-            else
-                Logger.info(event.getPlayer().getUsername() + " (" + event.getPlayer().getUuid() + ") joined.");
-            player.sendMessage("Hello!");
-        });
+                Logger.info(STR."\{event.getPlayer().getUsername()} (\{event.getPlayer().getUuid()}) joined with the ip: \{player.getPlayerConnection().getServerAddress()}");
+            else Logger.info(STR."\{event.getPlayer().getUsername()} (\{event.getPlayer().getUuid()}) joined.");
+        })));
     }
 }
