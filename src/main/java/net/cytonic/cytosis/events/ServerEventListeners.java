14c7313b57d08baf0316bafd39af42a4b8b4e514
webhead1104 <minecraft1234new@hotmail.com>
2024-05-06 16:16:23 -0500

    Add Player leave event and log chat to console

--- src/main/java/net/cytonic/cytosis/events/ServerEventListeners.java
+++ src/main/java/net/cytonic/cytosis/events/ServerEventListeners.java
@@ -7,6 +7,7 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.player.PlayerChatEvent;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 
 public class ServerEventListeners {
@@ -16,7 +17,7 @@ public class ServerEventListeners {
         Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-configuration", true, 1, AsyncPlayerConfigurationEvent.class, (event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(Cytosis.getDefaultInstance());
-            player.setRespawnPoint(new Pos(0, 45, 0));
+            player.setRespawnPoint(new Pos(0, 1, 0));
         })));
         Logger.info("Registering player spawn event.");
         Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-spawn", false, 1, PlayerSpawnEvent.class, (event -> {
@@ -25,11 +26,17 @@ public class ServerEventListeners {
                 Logger.info(STR."\{event.getPlayer().getUsername()} (\{event.getPlayer().getUuid()}) joined with the ip: \{player.getPlayerConnection().getServerAddress()}");
             else Logger.info(STR."\{event.getPlayer().getUsername()} (\{event.getPlayer().getUuid()}) joined.");
         })));
+        Logger.info("Registering player leave event.");
+        Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-leave",false,1, PlayerDisconnectEvent.class, (event -> {
+            final Player player = event.getPlayer();
+            Logger.info(STR."\{event.getPlayer().getUsername()} (\{event.getPlayer().getUuid()}) left.");
+        })));
         Logger.info("Registering player chat event.");
-        Cytosis.getEventHandler().registerListener(new EventListener<PlayerChatEvent>("core:player-chat", false, 1, PlayerChatEvent.class,event ->{
+        Cytosis.getEventHandler().registerListener(new EventListener<>("core:player-chat", false, 1, PlayerChatEvent.class, event -> {
             final Player player = event.getPlayer();
             if (CytosisSettings.LOG_PLAYER_CHAT)
-                Cytosis.getDatabaseManager().getDatabase().addChat(player.getUuid(),event.getMessage());
+                Cytosis.getDatabaseManager().getDatabase().addChat(player.getUuid(), event.getMessage());
+            Logger.info(STR."\{player.getUsername()}: \{event.getMessage()}");
         }));
     }
 }
