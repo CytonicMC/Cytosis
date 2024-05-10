8878116de2473fe8957d3aa12aa17b2ae996b8c7
webhead1104 <minecraft1234new@hotmail.com>
2024-05-10 13:20:52 -0500

    Delete duplicated file and remove lines

--- src/main/java/net/cytonic/cytosis/ranks/PlayerRank.java
+++ src/main/java/net/cytonic/cytosis/ranks/PlayerRank.java
@@ -3,7 +3,6 @@ package net.cytonic.cytosis.ranks;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-
 import static net.cytonic.cytosis.utils.MiniMessageTemplate.MM;
 
 /**
