a6a44f7c2b8241d466421166d5c6fd44c1b5b240
foxikle <tristantroop260@gmail.com>
2024-06-04 18:53:41 -0400

    Update settings

--- src/main/java/net/cytonic/cytosis/config/CytosisSettings.java
+++ src/main/java/net/cytonic/cytosis/config/CytosisSettings.java
@@ -1,6 +1,7 @@
 package net.cytonic.cytosis.config;
 
 import net.cytonic.cytosis.logging.Logger;
+
 import java.util.Map;
 
 /**
@@ -13,6 +14,7 @@ public class CytosisSettings {
     public static boolean LOG_PLAYER_QUITS = true;
     public static boolean LOG_PLAYER_COMMANDS = true;
     public static boolean LOG_PLAYER_CHAT = true;
+
     // Database
     public static boolean DATABASE_ENABLED = true;
     public static String DATABASE_USER = "";
@@ -21,16 +23,23 @@ public class CytosisSettings {
     public static int DATABASE_PORT = 3306;
     public static String DATABASE_NAME = "";
     public static boolean DATABASE_USE_SSL = false;
+
     // server
     public static boolean SERVER_PROXY_MODE = false;
     public static String SERVER_SECRET = "";
     public static int SERVER_PORT = 25565;
+
     // RabbitMQ
     public static boolean RABBITMQ_ENABLED = false;
     public static String RABBITMQ_HOST = "";
     public static String RABBITMQ_PASSWORD = "";
     public static String RABBITMQ_USERNAME = "";
     public static int RABBITMQ_PORT = 5672;
+
+    //Redis
+    public static int REDIS_PORT = 6379;
+    public static String REDIS_HOST = "";
+    public static String REDIS_PASSWORD = "";
 
     public static void inportConfig(Map<String, Object> config) {
         Logger.info("Importing config!");
@@ -65,6 +74,11 @@ public class CytosisSettings {
                     case "rabbitmq.port" -> RABBITMQ_PORT = toInt(value);
                     case "rabbitmq.enabled" -> RABBITMQ_ENABLED = (boolean) value;
 
+                    // Redis
+                    case "redis.port" -> REDIS_PORT = toInt(value);
+                    case "redis.host" -> REDIS_HOST = (String) value;
+                    case "redis.password" -> REDIS_PASSWORD = (String) value;
+
                     default -> { /*Do nothing*/ }
                 }
             } catch (ClassCastException e) {
@@ -81,55 +95,90 @@ public class CytosisSettings {
     public static void loadEnvironmentVariables() {
         Logger.info("Loading environment variables!");
         // logging
-        if (!(System.getenv("LOG_PLAYER_IPS") == null)) CytosisSettings.LOG_PLAYER_IPS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_IPS"));
-        if (!(System.getenv("LOG_PLAYER_JOINS") == null)) CytosisSettings.LOG_PLAYER_JOINS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_JOINS"));
-        if (!(System.getenv("LOG_PLAYER_QUITS") == null)) CytosisSettings.LOG_PLAYER_QUITS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_QUITS"));
-        if (!(System.getenv("LOG_PLAYER_COMMANDS") == null)) CytosisSettings.LOG_PLAYER_COMMANDS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_COMMANDS"));
-        if (!(System.getenv("LOG_PLAYER_CHAT") == null)) CytosisSettings.LOG_PLAYER_CHAT = Boolean.parseBoolean(System.getenv("LOG_PLAYER_CHAT"));
+        if (!(System.getenv("LOG_PLAYER_IPS") == null))
+            LOG_PLAYER_IPS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_IPS"));
+        if (!(System.getenv("LOG_PLAYER_JOINS") == null))
+            LOG_PLAYER_JOINS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_JOINS"));
+        if (!(System.getenv("LOG_PLAYER_QUITS") == null))
+            LOG_PLAYER_QUITS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_QUITS"));
+        if (!(System.getenv("LOG_PLAYER_COMMANDS") == null))
+            LOG_PLAYER_COMMANDS = Boolean.parseBoolean(System.getenv("LOG_PLAYER_COMMANDS"));
+        if (!(System.getenv("LOG_PLAYER_CHAT") == null))
+            LOG_PLAYER_CHAT = Boolean.parseBoolean(System.getenv("LOG_PLAYER_CHAT"));
         // database
-        if (!(System.getenv("DATABASE_ENABLED") == null)) CytosisSettings.DATABASE_ENABLED = Boolean.parseBoolean(System.getenv("DATABASE_ENABLED"));
-        if (!(System.getenv("DATABASE_USER") == null)) CytosisSettings.DATABASE_USER = System.getenv("DATABASE_USER");
-        if (!(System.getenv("DATABASE_PASSWORD") == null)) CytosisSettings.DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
-        if (!(System.getenv("DATABASE_HOST") == null)) CytosisSettings.DATABASE_HOST = System.getenv("DATABASE_HOST");
-        if (!(System.getenv("DATABASE_PORT") == null)) CytosisSettings.DATABASE_PORT = Integer.parseInt((System.getenv("DATABASE_PORT")));
-        if (!(System.getenv("DATABASE_NAME") == null)) CytosisSettings.DATABASE_NAME = System.getenv("DATABASE_NAME");
-        if (!(System.getenv("DATABASE_USE_SSL") == null)) CytosisSettings.DATABASE_USE_SSL = Boolean.parseBoolean(System.getenv("DATABASE_USE_SSL"));
+        if (!(System.getenv("DATABASE_ENABLED") == null))
+            DATABASE_ENABLED = Boolean.parseBoolean(System.getenv("DATABASE_ENABLED"));
+        if (!(System.getenv("DATABASE_USER") == null)) DATABASE_USER = System.getenv("DATABASE_USER");
+        if (!(System.getenv("DATABASE_PASSWORD") == null)) DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
+        if (!(System.getenv("DATABASE_HOST") == null)) DATABASE_HOST = System.getenv("DATABASE_HOST");
+        if (!(System.getenv("DATABASE_PORT") == null))
+            DATABASE_PORT = Integer.parseInt((System.getenv("DATABASE_PORT")));
+        if (!(System.getenv("DATABASE_NAME") == null)) DATABASE_NAME = System.getenv("DATABASE_NAME");
+        if (!(System.getenv("DATABASE_USE_SSL") == null))
+            DATABASE_USE_SSL = Boolean.parseBoolean(System.getenv("DATABASE_USE_SSL"));
         //server
-        if (!(System.getenv("SERVER_PROXY_MODE") == null)) CytosisSettings.SERVER_PROXY_MODE = Boolean.parseBoolean(System.getenv("SERVER_PROXY_MODE"));
-        if (!(System.getenv("SERVER_SECRET") == null)) CytosisSettings.SERVER_SECRET = System.getenv("SERVER_SECRET");
-        if (!(System.getenv("SERVER_PORT") == null)) CytosisSettings.SERVER_PORT = Integer.parseInt(System.getenv("SERVER_PORT"));
+        if (!(System.getenv("SERVER_PROXY_MODE") == null))
+            SERVER_PROXY_MODE = Boolean.parseBoolean(System.getenv("SERVER_PROXY_MODE"));
+        if (!(System.getenv("SERVER_SECRET") == null)) SERVER_SECRET = System.getenv("SERVER_SECRET");
+        if (!(System.getenv("SERVER_PORT") == null)) SERVER_PORT = Integer.parseInt(System.getenv("SERVER_PORT"));
         // RabbitMQ
-        if (!(System.getenv("RABBITMQ_ENABLED") == null)) CytosisSettings.RABBITMQ_ENABLED = Boolean.parseBoolean(System.getenv("RABBITMQ_ENABLED"));
-        if (!(System.getenv("RABBITMQ_HOST") == null)) CytosisSettings.RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
-        if (!(System.getenv("RABBITMQ_PASSWORD") == null)) CytosisSettings.RABBITMQ_PASSWORD = System.getenv("RABBITMQ_PASSWORD");
-        if (!(System.getenv("RABBITMQ_USERNAME") == null)) CytosisSettings.RABBITMQ_USERNAME = System.getenv("RABBITMQ_USERNAME");
-        if (!(System.getenv("RABBITMQ_PORT") == null)) CytosisSettings.RABBITMQ_PORT = Integer.parseInt(System.getenv("RABBITMQ_PORT"));
+        if (!(System.getenv("RABBITMQ_ENABLED") == null))
+            RABBITMQ_ENABLED = Boolean.parseBoolean(System.getenv("RABBITMQ_ENABLED"));
+        if (!(System.getenv("RABBITMQ_HOST") == null)) RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
+        if (!(System.getenv("RABBITMQ_PASSWORD") == null)) RABBITMQ_PASSWORD = System.getenv("RABBITMQ_PASSWORD");
+        if (!(System.getenv("RABBITMQ_USERNAME") == null)) RABBITMQ_USERNAME = System.getenv("RABBITMQ_USERNAME");
+        if (!(System.getenv("RABBITMQ_PORT") == null)) RABBITMQ_PORT = Integer.parseInt(System.getenv("RABBITMQ_PORT"));
+
+        // redis
+        if (!(System.getenv("REDIS_HOST") == null)) REDIS_HOST = System.getenv("REDIS_HOST");
+        if (!(System.getenv("REDIS_PORT") == null)) REDIS_PORT = Integer.parseInt(System.getenv("REDIS_PORT"));
+        if (!(System.getenv("REDIS_PASSWORD") == null)) REDIS_PASSWORD = System.getenv("REDIS_PASSWORD");
     }
 
     public static void loadCommandArgs() {
         Logger.info("Loading command args!");
         // logging
-        if (!(System.getProperty("LOG_PLAYER_IPS") == null)) CytosisSettings.LOG_PLAYER_IPS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_IPS"));
-        if (!(System.getProperty("LOG_PLAYER_JOINS") == null)) CytosisSettings.LOG_PLAYER_JOINS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_JOINS"));
-        if (!(System.getProperty("LOG_PLAYER_QUITS") == null)) CytosisSettings.LOG_PLAYER_QUITS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_QUITS"));
-        if (!(System.getProperty("LOG_PLAYER_COMMANDS") == null)) CytosisSettings.LOG_PLAYER_COMMANDS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_COMMANDS"));
-        if (!(System.getProperty("LOG_PLAYER_CHAT") == null)) CytosisSettings.LOG_PLAYER_CHAT = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_CHAT"));
+        if (!(System.getProperty("LOG_PLAYER_IPS") == null))
+            LOG_PLAYER_IPS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_IPS"));
+        if (!(System.getProperty("LOG_PLAYER_JOINS") == null))
+            LOG_PLAYER_JOINS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_JOINS"));
+        if (!(System.getProperty("LOG_PLAYER_QUITS") == null))
+            LOG_PLAYER_QUITS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_QUITS"));
+        if (!(System.getProperty("LOG_PLAYER_COMMANDS") == null))
+            LOG_PLAYER_COMMANDS = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_COMMANDS"));
+        if (!(System.getProperty("LOG_PLAYER_CHAT") == null))
+            LOG_PLAYER_CHAT = Boolean.parseBoolean(System.getProperty("LOG_PLAYER_CHAT"));
         // database
-        if (!(System.getProperty("DATABASE_USER") == null)) CytosisSettings.DATABASE_USER = System.getProperty("DATABASE_USER");
-        if (!(System.getProperty("DATABASE_PASSWORD") == null)) CytosisSettings.DATABASE_PASSWORD = System.getProperty("DATABASE_PASSWORD");
-        if (!(System.getProperty("DATABASE_HOST") == null)) CytosisSettings.DATABASE_HOST = System.getProperty("DATABASE_HOST");
-        if (!(System.getProperty("DATABASE_PORT") == null)) CytosisSettings.DATABASE_PORT = Integer.parseInt((System.getProperty("DATABASE_PORT")));
-        if (!(System.getProperty("DATABASE_NAME") == null)) CytosisSettings.DATABASE_NAME = System.getProperty("DATABASE_NAME");
-        if (!(System.getProperty("DATABASE_USE_SSL") == null)) CytosisSettings.DATABASE_USE_SSL = Boolean.parseBoolean(System.getProperty("DATABASE_USE_SSL"));
+        if (!(System.getProperty("DATABASE_USER") == null)) DATABASE_USER = System.getProperty("DATABASE_USER");
+        if (!(System.getProperty("DATABASE_PASSWORD") == null))
+            DATABASE_PASSWORD = System.getProperty("DATABASE_PASSWORD");
+        if (!(System.getProperty("DATABASE_HOST") == null)) DATABASE_HOST = System.getProperty("DATABASE_HOST");
+        if (!(System.getProperty("DATABASE_PORT") == null))
+            DATABASE_PORT = Integer.parseInt((System.getProperty("DATABASE_PORT")));
+        if (!(System.getProperty("DATABASE_NAME") == null)) DATABASE_NAME = System.getProperty("DATABASE_NAME");
+        if (!(System.getProperty("DATABASE_USE_SSL") == null))
+            DATABASE_USE_SSL = Boolean.parseBoolean(System.getProperty("DATABASE_USE_SSL"));
         //server
-        if (!(System.getProperty("SERVER_PROXY_MODE") == null)) CytosisSettings.SERVER_PROXY_MODE = Boolean.parseBoolean(System.getProperty("SERVER_PROXY_MODE"));
-        if (!(System.getProperty("SERVER_SECRET") == null)) CytosisSettings.SERVER_SECRET = System.getProperty("SERVER_SECRET");
-        if (!(System.getProperty("SERVER_PORT") == null)) CytosisSettings.SERVER_PORT = Integer.parseInt(System.getProperty("SERVER_PORT"));
+        if (!(System.getProperty("SERVER_PROXY_MODE") == null))
+            SERVER_PROXY_MODE = Boolean.parseBoolean(System.getProperty("SERVER_PROXY_MODE"));
+        if (!(System.getProperty("SERVER_SECRET") == null)) SERVER_SECRET = System.getProperty("SERVER_SECRET");
+        if (!(System.getProperty("SERVER_PORT") == null))
+            SERVER_PORT = Integer.parseInt(System.getProperty("SERVER_PORT"));
         // RabbitMQ
-        if (!(System.getProperty("RABBITMQ_ENABLED") == null)) CytosisSettings.RABBITMQ_ENABLED = Boolean.parseBoolean(System.getProperty("RABBITMQ_ENABLED"));
-        if (!(System.getProperty("RABBITMQ_HOST") == null)) CytosisSettings.RABBITMQ_HOST = System.getProperty("RABBITMQ_HOST");
-        if (!(System.getProperty("RABBITMQ_PASSWORD") == null)) CytosisSettings.RABBITMQ_PASSWORD = System.getProperty("RABBITMQ_PASSWORD");
-        if (!(System.getProperty("RABBITMQ_USERNAME") == null)) CytosisSettings.RABBITMQ_USERNAME = System.getProperty("RABBITMQ_USERNAME");
-        if (!(System.getProperty("RABBITMQ_PORT") == null)) CytosisSettings.RABBITMQ_PORT = Integer.parseInt(System.getProperty("RABBITMQ_PORT"));
+        if (!(System.getProperty("RABBITMQ_ENABLED") == null))
+            RABBITMQ_ENABLED = Boolean.parseBoolean(System.getProperty("RABBITMQ_ENABLED"));
+        if (!(System.getProperty("RABBITMQ_HOST") == null)) RABBITMQ_HOST = System.getProperty("RABBITMQ_HOST");
+        if (!(System.getProperty("RABBITMQ_PASSWORD") == null))
+            RABBITMQ_PASSWORD = System.getProperty("RABBITMQ_PASSWORD");
+        if (!(System.getProperty("RABBITMQ_USERNAME") == null))
+            RABBITMQ_USERNAME = System.getProperty("RABBITMQ_USERNAME");
+        if (!(System.getProperty("RABBITMQ_PORT") == null))
+            RABBITMQ_PORT = Integer.parseInt(System.getProperty("RABBITMQ_PORT"));
+
+        // redis
+        if (!(System.getProperty("REDIS_HOST") == null)) REDIS_HOST = System.getProperty("REDIS_HOST");
+        if (!(System.getProperty("REDIS_PORT") == null))
+            REDIS_PORT = Integer.parseInt(System.getProperty("REDIS_PORT"));
+        if (!(System.getProperty("REDIS_PASSWORD") == null)) REDIS_PASSWORD = System.getProperty("REDIS_PASSWORD");
     }
 }
