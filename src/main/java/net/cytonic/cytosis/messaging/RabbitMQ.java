2922617109d27b6b2e71b3fbe13ce1f0ac9726dc
foxikle <tristantroop260@gmail.com>
2024-06-04 18:51:03 -0400

    Update methods to message more things

--- src/main/java/net/cytonic/cytosis/messaging/RabbitMQ.java
+++ src/main/java/net/cytonic/cytosis/messaging/RabbitMQ.java
@@ -1,5 +1,16 @@
 package net.cytonic.cytosis.messaging;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import net.cytonic.cytosis.Cytosis;
+import net.cytonic.cytosis.config.CytosisSettings;
+import net.cytonic.cytosis.logging.Logger;
+import net.cytonic.cytosis.utils.OfflinePlayer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
+import net.minestom.server.entity.Player;
+
 import java.io.IOException;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
@@ -36,6 +47,7 @@ public class RabbitMQ {
     public static final String CHAT_CHANNEL_QUEUE = "chat-channel";
     public static final String PLAYER_KICK_QUEUE = "player-kick";
     public static final String CHAT_CHANNEL_EXCHANGE = "chat-exchange";
+    public static final String PLAYER_KICK_QUEUE = "player-kick";
     private Connection connection;
     private Channel channel;
 
@@ -224,6 +236,54 @@ public class RabbitMQ {
             channel.basicConsume(CHAT_CHANNEL_QUEUE, true, deliverCallback, _ -> {});
         } catch (IOException e) {
             Logger.error("error", e);
+        }
+    }
+}
+
+
+    public void sendServerShutdownMessage() {
+        //formatting: {server-name}|:|{server-ip}|:|{server-port}
+        String serverIP;
+        try {
+            serverIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            Logger.error("An error occoured whilst fetching this server's IP address! Bailing out!", e);
+            return;
+        }
+        String message = STR."\{Cytosis.SERVER_ID}|:|\{serverIP}|:|\{CytosisSettings.SERVER_PORT}";
+        try {
+            channel.basicPublish("", SHUTDOWN_QUEUE, null, message.getBytes());
+        } catch (IOException e) {
+            Logger.error("An error occoured whilst attempting to send the server declaration message!", e);
+        }
+        Logger.info(STR."Server Declaration message sent! '\{message}'.");
+    }
+
+    public void shutdown() {
+        try {
+            connection.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void kickPlayer(Player player, KickReason reason, Component message) {
+        // FORMAT: {uuid}|:|{reason}|:|{name}|:|{message}|:|{rescuable}
+        String rawMessage = STR."\{player.getUuid().toString()}|:|\{reason}|:|\{player.getUsername()}|:|\{JSONComponentSerializer.json().serialize(message)}|:|\{reason.isRescuable()}";
+        try {
+            channel.basicPublish("", PLAYER_KICK_QUEUE, null, rawMessage.getBytes());
+        } catch (IOException e) {
+            Logger.error(STR."An error occoured whilst attempting to kick the player \{player.getName()}.", e);
+        }
+    }
+
+    public void kickPlayer(OfflinePlayer player, KickReason reason, Component message) {
+        // FORMAT: {uuid}|:|{reason}|:|{name}|:|{message}|:|{rescuable}
+        String rawMessage = STR."\{player.uuid().toString()}|:|\{reason}|:|\{player.name()}|:|\{JSONComponentSerializer.json().serialize(message)}|:|\{reason.isRescuable()}";
+        try {
+            channel.basicPublish("", PLAYER_KICK_QUEUE, null, rawMessage.getBytes());
+        } catch (IOException e) {
+            Logger.error(STR."An error occoured whilst attempting to kick the player \{player.name()}.", e);
         }
     }
 }
