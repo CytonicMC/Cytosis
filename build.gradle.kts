06b92e230e08720ce74c49929582416e13a277a3
foxikle <tristantroop260@gmail.com>
2024-06-12 17:48:36 -0400

    ok I give up

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,8 +62,7 @@ tasks {
         mergeServiceFiles()
         archiveFileName.set("cytosis.jar")
         archiveClassifier.set("")
-        println("server dir: " + System.getenv("SERVER_DIR"))
-        destinationDirectory.set(file(providers.gradleProperty("server_dir").orElse(System.getenv("SERVER_DIR"))))
+        destinationDirectory.set(file(providers.gradleProperty("server_dir").orElse("/home/runner/work/Cytosis/build")))
     }
 }
 
