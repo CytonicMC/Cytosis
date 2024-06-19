21f70185b87434fa2f67b26e88fa69b6c59adab9
Webhead1104 <minecraft1234new@hotmail.com>
2024-06-19 16:38:06 -0500

    More stuff for the stop command

--- src/main/java/net/cytonic/cytosis/commands/StopCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/StopCommand.java
@@ -1,7 +1,10 @@
 package net.cytonic.cytosis.commands;
 
+import net.cytonic.cytosis.utils.MiniMessageTemplate;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.suggestion.SuggestionEntry;
 
 /**
  * A class representing the stop command
@@ -14,9 +17,17 @@ public class StopCommand extends Command {
     public StopCommand() {
         super("stop");
         setCondition((sender, _) -> sender.hasPermission("cytosis.commands.stop"));
-        addSyntax((sender, _) -> {
-            if (sender.hasPermission("cytonic.commands.stop"))
-                MinecraftServer.stopCleanly();
+        var areYouSureArgument = ArgumentType.Word("areyousure").from("confirm");
+        areYouSureArgument.setSuggestionCallback(((_, _, suggestion) -> suggestion.addEntry(new SuggestionEntry("confirm"))));
+        addSyntax((sender, context) -> {
+            if (sender.hasPermission("cytonic.commands.stop")) {
+                if (context.get(areYouSureArgument).equalsIgnoreCase("confirm")) {
+                    sender.sendMessage("Stopping the server...");
+                    MinecraftServer.stopCleanly();
+                } else {
+                    sender.sendMessage(MiniMessageTemplate.MM."<RED>Are you sure you want to stop the server? If so add confirm to the command");
+                }
+            }
         });
     }
 }
