5fb2f81d6c62616d06bd843d1903305f44b20745
RespDev <mail.nathan.flowers@gmail.com>
2024-07-05 12:12:20 -0500

    Made lightning effect last longer and added sound effect.

--- src/main/java/net/cytonic/cytosis/commands/KaboomCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/KaboomCommand.java
@@ -1,18 +1,18 @@
 package net.cytonic.cytosis.commands;
 
 import net.cytonic.cytosis.Cytosis;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
-import net.minestom.server.effects.Effects;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.sound.SoundEvent;
 import net.minestom.server.timer.Scheduler;
 import net.minestom.server.timer.TaskSchedule;
-
 import static net.cytonic.cytosis.utils.MiniMessageTemplate.MM;
 
 /**
@@ -38,12 +38,13 @@ public class KaboomCommand extends Command {
                         Pos spawnPosition = online.getPosition();
                         Entity lightning = new Entity(EntityType.LIGHTNING_BOLT);
                         lightning.setInstance(instance, spawnPosition);
+                        online.playSound(Sound.sound(SoundEvent.ENTITY_LIGHTNING_BOLT_THUNDER, Sound.Source.WEATHER, 1f, 1f));
 
                         // Remove lightning
                         Scheduler scheduler = MinecraftServer.getSchedulerManager();
                         scheduler.submitTask(() -> {
                             lightning.remove();
-                            return TaskSchedule.seconds(1);
+                            return TaskSchedule.seconds(2);
                         });
 
                         // Launch player
