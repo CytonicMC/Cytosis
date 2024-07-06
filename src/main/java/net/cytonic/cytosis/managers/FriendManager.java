67933286cd74877225111120636a375be4e7fba5
foxikle <tristantroop260@gmail.com>
2024-07-06 16:52:51 -0400

    javadocs

--- src/main/java/net/cytonic/cytosis/managers/FriendManager.java
+++ src/main/java/net/cytonic/cytosis/managers/FriendManager.java
@@ -14,14 +14,34 @@ import java.util.concurrent.ConcurrentHashMap;
 import static net.cytonic.cytosis.data.DatabaseTemplate.QUERY;
 import static net.cytonic.cytosis.data.DatabaseTemplate.UPDATE;
 
+/**
+ * A class to manage friends
+ */
 public class FriendManager {
-    public ConcurrentHashMap<UUID, List<UUID>> friends = new ConcurrentHashMap<>();
 
+    /**
+     * The default constructor
+     */
+    public FriendManager() {
+        // do nothing
+    }
+
+    private final ConcurrentHashMap<UUID, List<UUID>> friends = new ConcurrentHashMap<>();
+
+    /**
+     * Gets a player's friends
+     *
+     * @param uuid The player
+     * @return the list of UUIDs of the player friends
+     */
     @Nullable
     public List<UUID> getFriends(UUID uuid) {
         return friends.get(uuid);
     }
 
+    /**
+     * Initializes the friends table and loads the online players friends
+     */
     public void init() {
         UPDATE."CREATE TABLE IF NOT EXISTS cytonic_friends (uuid VARCHAR(36), friends TEXT)".whenComplete((_, throwable) -> {
             if (throwable != null) Logger.error("An error occurred whilst creating the friends table!", throwable);
@@ -29,6 +49,10 @@ public class FriendManager {
         Cytosis.getOnlinePlayers().forEach(player -> loadFriends(player.getUuid()));
     }
 
+    /**
+     * Loads a player's friends into memory
+     * @param uuid The player
+     */
     public void loadFriends(UUID uuid) {
         QUERY."SELECT friends FROM friends WHERE uuid = '\{uuid.toString()}'".whenComplete((resultSet, throwable) -> {
             if (throwable != null) {
@@ -47,6 +71,11 @@ public class FriendManager {
         });
     }
 
+    /**
+     * Adds a player as a friend
+     * @param uuid The player
+     * @param friend The friend
+     */
     public void addFriend(UUID uuid, UUID friend) {
         List<UUID> list = friends.getOrDefault(uuid, new ArrayList<>());
         list.add(friend);
@@ -55,6 +84,24 @@ public class FriendManager {
         UPDATE."INSERT INTO cytonic_friends (uuid, friends) VALUES ('\{uuid}', '\{Cytosis.GSON.toJson(list)}') ON DUPLICATE KEY UPDATE friends = '\{Cytosis.GSON.toJson(list)}')".whenComplete((_, throwable) -> {
             if (throwable != null) {
                 Logger.error("An error occurred whilst adding a friend!", throwable);
+            }
+        });
+    }
+
+    /**
+     * Adds a player as a friend
+     *
+     * @param uuid   The player
+     * @param friend The friend
+     */
+    public void removeFriend(UUID uuid, UUID friend) {
+        List<UUID> list = friends.getOrDefault(uuid, new ArrayList<>());
+        list.remove(friend);
+        friends.put(uuid, list);
+
+        UPDATE."INSERT INTO cytonic_friends (uuid, friends) VALUES ('\{uuid}', '\{Cytosis.GSON.toJson(list)}') ON DUPLICATE KEY UPDATE friends = '\{Cytosis.GSON.toJson(list)}')".whenComplete((_, throwable) -> {
+            if (throwable != null) {
+                Logger.error("An error occurred whilst removing a friend!", throwable);
             }
         });
     }
