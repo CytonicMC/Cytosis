54a1bc02bffcfd846d878a72e47ceff61a526116
foxikle <tristantroop260@gmail.com>
2024-07-12 10:26:56 -0400

    Add join/leave messages for friends

--- src/main/java/net/cytonic/cytosis/messaging/pubsub/PlayerLoginLogout.java
+++ src/main/java/net/cytonic/cytosis/messaging/pubsub/PlayerLoginLogout.java
@@ -34,9 +34,11 @@ public class PlayerLoginLogout extends JedisPubSub {
         if (parts[2].equalsIgnoreCase("JOIN")) {
             network.addPlayer(parts[0], UUID.fromString(parts[1]));
             Cytosis.getPreferenceManager().loadPlayerPreferences(UUID.fromString(parts[1]));
+            Cytosis.getFriendManager().sendLoginMessage(UUID.fromString(parts[1]));
         } else if (parts[2].equalsIgnoreCase("LEAVE")) {
             network.removePlayer(parts[0], UUID.fromString(parts[1]));
             Cytosis.getPreferenceManager().unloadPlayerPreferences(UUID.fromString(parts[1]));
+            Cytosis.getFriendManager().sendLogoutMessage(UUID.fromString(parts[1]));
         }
     }
 }
