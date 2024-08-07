599b784c1d52d167b40712268db551fd1bd073f9
foxikle <tristantroop260@gmail.com>
2024-08-07 14:17:13 -0400

    create a field with all the preferences for easy querying.

--- src/main/java/net/cytonic/cytosis/data/enums/CytosisPreferences.java
+++ src/main/java/net/cytonic/cytosis/data/enums/CytosisPreferences.java
@@ -2,36 +2,39 @@ package net.cytonic.cytosis.data.enums;
 
 import net.cytonic.enums.ChatChannel;
 import net.cytonic.objects.NamespacedPreference;
-import net.minestom.server.utils.NamespaceID;
+
+import java.util.Set;
 
 /**
  * A list of Cytosis supplied preferences
  */
 public class CytosisPreferences {
     /**
+     * A preference to accept or decline friend requests, type of BOOLEAN
+     */
+    public static final NamespacedPreference<Boolean> ACCEPT_FRIEND_REQUESTS = new NamespacedPreference<>(CytosisNamespaces.ACCEPT_FRIEND_REQUESTS, true);
+    /**
+     * A preference to send or not send server alerts, type of BOOLEAN
+     */
+    public static final NamespacedPreference<Boolean> SERVER_ALERTS = new NamespacedPreference<>(CytosisNamespaces.SERVER_ALERTS, false);
+    /**
+     * A preference to store the players chat channel, type of ChatChannel
+     */
+    public static final NamespacedPreference<ChatChannel> CHAT_CHANNEL = new NamespacedPreference<>(CytosisNamespaces.CHAT_CHANNEL, ChatChannel.ALL);
+    /**
+     * A preference if the player is vanished, type of BOOLEAN
+     */
+    public static final NamespacedPreference<Boolean> VANISHED = new NamespacedPreference<>(CytosisNamespaces.VANISHED, false);
+
+    /**
+     * A set of all the preferences that are available here.
+     */
+    public static final Set<NamespacedPreference<?>> ALL = Set.of(ACCEPT_FRIEND_REQUESTS, SERVER_ALERTS, CHAT_CHANNEL, VANISHED);
+
+    /**
      * A private constructor to prevent instantiation
      */
     private CytosisPreferences() {
         // do nothing
     }
-
-    /**
-     * A preference to accept or decline friend requests, type of BOOLEAN
-     */
-    public static final NamespacedPreference<Boolean> ACCEPT_FRIEND_REQUESTS = new NamespacedPreference<>(NamespaceID.from("cytosis", "accept_friend_request"), true);
-
-    /**
-     * A preference to send or not send server alerts, type of BOOLEAN
-     */
-    public static final NamespacedPreference<Boolean> SERVER_ALERTS = new NamespacedPreference<>(NamespaceID.from("cytosis", "server_alerts"), false);
-
-    /**
-     * A preference to store the players chat channel, type of ChatChannel
-     */
-    public static final NamespacedPreference<ChatChannel> CHAT_CHANNEL = new NamespacedPreference<>(NamespaceID.from("cytosis", "chat_channel"), ChatChannel.ALL);
-
-    /**
-     * A preference if the player is vanished, type of BOOLEAN
-     */
-    public static final NamespacedPreference<Boolean> VANISHED = new NamespacedPreference<>(NamespaceID.from("cytosis", "vanished"), false);
 }
