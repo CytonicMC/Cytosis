ce980e106f36175d6a6304bcf33354e91bd097e6
Webhead1104 <minecraft1234new@hotmail.com>
2024-08-17 14:02:10 -0500

    Vanish Command

--- src/main/java/net/cytonic/cytosis/managers/VanishManager.java
+++ src/main/java/net/cytonic/cytosis/managers/VanishManager.java
@@ -4,12 +4,8 @@ import net.cytonic.cytosis.Cytosis;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.entity.Metadata;
 import net.minestom.server.entity.Player;
-import net.minestom.server.network.packet.server.play.EntityEffectPacket;
 import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
-import net.minestom.server.network.packet.server.play.RemoveEntityEffectPacket;
 import net.minestom.server.network.packet.server.play.TeamsPacket;
-import net.minestom.server.potion.Potion;
-import net.minestom.server.potion.PotionEffect;
 
 import java.util.List;
 import java.util.Map;
@@ -33,17 +29,15 @@ public class VanishManager {
      *
      * @param player the player to vanish
      */
+    @SuppressWarnings("UnstableApiUsage")
     public void enableVanish(Player player) {
         player.updateViewableRule(p -> {
             if (p.hasPermission("cytosis.vanish.can_see_vanished")) {
                 TeamsPacket packet = new TeamsPacket("vanished", new TeamsPacket.CreateTeamAction(MM."",
                         (byte) 0x02, TeamsPacket.NameTagVisibility.HIDE_FOR_OTHER_TEAMS, TeamsPacket.CollisionRule.NEVER,
                         NamedTextColor.GRAY, MM."<gray><b>VANISHED! ", MM."", List.of(p.getUsername(), player.getUsername())));
-                EntityEffectPacket effect = new EntityEffectPacket(player.getEntityId(),
-                        new Potion(PotionEffect.GLOWING, (byte) 0,  999999999));
-                EntityMetaDataPacket invis = new EntityMetaDataPacket(player.getEntityId(), Map.of(0, Metadata.Byte((byte) 0x20)));
-                        p.setInvisible(true);
-                p.sendPackets(packet, effect);
+                EntityMetaDataPacket invis = new EntityMetaDataPacket(player.getEntityId(), Map.of(0, Metadata.Byte((byte) (0x20 | 0x40))));
+                p.sendPackets(packet, invis);
                 return true;
             }
             return false;
@@ -58,6 +52,11 @@ public class VanishManager {
      */
     @SuppressWarnings("UnstableApiUsage")
     public void disableVanish(Player player) {
-        player.updateViewableRule(_ -> true);
+        player.updateViewableRule(p -> {
+            EntityMetaDataPacket invis = new EntityMetaDataPacket(player.getEntityId(), Map.of(0, Metadata.Byte((byte) 0)));
+            p.sendPacket(invis);
+            Cytosis.getRankManager().setupCosmetics(player, Cytosis.getRankManager().getPlayerRank(player.getUuid()).orElseThrow());
+            return true;
+        });
     }
 }
