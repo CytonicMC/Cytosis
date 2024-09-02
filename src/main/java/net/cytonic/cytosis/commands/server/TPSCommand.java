52684bb9a39486d4abfe507fb978e9ee2fb4ba63
foxikle <tristantroop260@gmail.com>
2024-09-02 15:02:28 -0400

    Fix up TPS command

--- src/main/java/net/cytonic/cytosis/commands/server/TPSCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/server/TPSCommand.java
@@ -1,11 +1,8 @@
 package net.cytonic.cytosis.commands.server;
 
-import lombok.Getter;
-import net.minestom.server.MinecraftServer;
+import net.minestom.server.ServerFlag;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.monitoring.TickMonitor;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 import static net.cytonic.utils.MiniMessageTemplate.MM;
 
@@ -15,22 +12,14 @@ import static net.cytonic.utils.MiniMessageTemplate.MM;
 public class TPSCommand extends Command {
 
     /**
-     * Creates a new command and sets up the consumers and execution logic
-     */
-    @Getter
-    private static final AtomicReference<TickMonitor> lastTick = new AtomicReference<>();
-
-    /**
      * A command for getting the current server tps
      */
-    public TPSCommand() {
+    public TPSCommand(TickMonitor tickMonitor) {
         super("tps");
         setCondition((sender, _) -> sender.hasPermission("cytosis.commands.tps"));
         setDefaultExecutor((sender, _) -> {
             if (sender.hasPermission("cytosis.commands.tps")) {
-                TickMonitor monitor = lastTick.get();
-                double tickMs = monitor.getTickTime();
-                double tps = Math.min(MinecraftServer.TICK_PER_SECOND, Math.floor(1000 / tickMs));
+                double tps = Math.min(ServerFlag.SERVER_TICKS_PER_SECOND, Math.floor(1000 / tickMonitor.getTickTime()));
                 sender.sendMessage(MM."<YELLOW>The current server TPS is \{tps}.");
             }
         });
