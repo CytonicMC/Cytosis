c741e0c0d6d9c5b523a8fcd49e0f78d0ef58f226
foxikle <tristantroop260@gmail.com>
2024-12-15 15:02:10 -0500

    Add a bunch of subjects

--- src/main/java/net/cytonic/cytosis/messaging/nats/Subjects.java
+++ src/main/java/net/cytonic/cytosis/messaging/nats/Subjects.java
@@ -2,11 +2,17 @@ package net.cytonic.cytosis.messaging.nats;
 
 import net.cytonic.cytosis.Cytosis;
 
+/**
+ * A list of every subject used by the NATS server
+ */
 public class Subjects {
     public static final String SERVER_REGISTER = "servers.register";
     public static final String SERVER_SHUTDOWN = "servers.shutdown";
     public static final String SERVER_LIST = "servers.list";
     public static final String PROXY_START = "servers.proxy.startup";
+
+    public static final String PLAYER_LEAVE = "players.disconnect";
+    public static final String PLAYER_JOIN = "players.connect";
 
 
     public static final String HEALTH_CHECK = "health.check." + Cytosis.getRawID();
@@ -15,6 +21,11 @@ public class Subjects {
     public static final String FRIEND_REQUEST = "friends.request";
     public static final String FRIEND_ACCEPT = "friends.accept";
     public static final String FRIEND_DECLINE = "friends.decline";
+    public static final String FRIEND_ACCEPT_BY_ID = "friends.accept.by_id";
+    public static final String FRIEND_DECLINE_BY_ID = "friends.decline.by_id";
     public static final String FRIEND_ACCEPTANCE_NOTIFY = "friends.accept.notify";
     public static final String FRIEND_DECLINATION_NOTIFY = "friends.decline.notify";
+    public static final String FRIEND_EXPIRE_NOTIFY = "friends.expire.notify";
+    public static final String FRIEND_REQUEST_NOTIFY = "friends.request.notify";
+    public static final String FRIEND_REMOVED = "friends.removed";
 }
