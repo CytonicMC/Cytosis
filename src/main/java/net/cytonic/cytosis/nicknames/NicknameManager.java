5ed96e26a6f7a649e757421774581d78af8af0e2
foxikle <foxikle@cytonic.net>
2025-04-21 21:10:59 -0400

    Refactor nickname handling and enhance packet management
    Introduced improved nickname initialization by adding rank support and streamlined skin data handling. Added methods for sending nickname packets to all viewers and individual players to ensure proper display while avoiding detection. This change enhances modularity and prepares the system for future feature extensions.

--- src/main/java/net/cytonic/cytosis/nicknames/NicknameManager.java
+++ src/main/java/net/cytonic/cytosis/nicknames/NicknameManager.java
@@ -1,12 +1,14 @@
 package net.cytonic.cytosis.nicknames;
 
 import net.cytonic.cytosis.Cytosis;
+import net.cytonic.cytosis.data.enums.PlayerRank;
 import net.cytonic.cytosis.data.objects.Tuple;
 import net.cytonic.cytosis.player.CytosisPlayer;
+import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Metadata;
-import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
-import net.minestom.server.network.packet.server.play.PlayerInfoUpdatePacket;
+import net.minestom.server.entity.Player;
+import net.minestom.server.network.packet.server.play.*;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -26,25 +28,51 @@ public class NicknameManager {
         if (nicknames.containsKey(playerUuid)) return;
 
         CytosisPlayer player = Cytosis.getPlayer(playerUuid).get();
-        String nick = NicknameGenerator.generateUsername();
-        Tuple<String, String> skin = NicknameGenerator.generateSkin();
 
-        nicknames.put(playerUuid, new NicknameData(nick, skin.getSecond(), skin.getFirst()));
+        nicknames.put(playerUuid, new NicknameData(
+                NicknameGenerator.generateUsername(),
+                NicknameGenerator.generateRank(),
+                NicknameGenerator.generateSkin())
+        );
+        sendNicknamePacketstoAll(player, false);
+    }
 
-        var properties = new ArrayList<PlayerInfoUpdatePacket.Property>();
-
-        if (skin.getSecond() != null && skin.getFirst() != null) {
-            properties.add(new PlayerInfoUpdatePacket.Property("textures", skin.getFirst(), skin.getSecond()));
+    /**
+     * Sends the packets to the player to display their nickname.
+     *
+     * @param player      the player to "nickname"
+     * @param freshViewer if the player has no existing viewer
+     */
+    public void sendNicknamePacketstoAll(CytosisPlayer player, boolean freshViewer) {
+        for (Player viewer : player.getViewers()) {
+            sendNicknamePacketsToPlayer(player, (CytosisPlayer) viewer, freshViewer);
         }
-        var entry = new PlayerInfoUpdatePacket.Entry(playerUuid, nick, properties, true,
-                0, GameMode.SURVIVAL, null, null, -1);
+    }
 
+    public void sendNicknamePacketsToPlayer(CytosisPlayer player, CytosisPlayer target, boolean freshViewer) {
+        var properties = new ArrayList<PlayerInfoUpdatePacket.Property>();
+        NicknameData data = getData(player.getUuid());
+        if (data == null) return;
+        if (data.signature() != null && data.value() != null) {
+            properties.add(new PlayerInfoUpdatePacket.Property("textures", data.value(), data.signature()));
+        }
+        var entry = new PlayerInfoUpdatePacket.Entry(player.getUuid(), data.nickname(), properties, false,
+                0, GameMode.SURVIVAL, null, null, 1);
 
-        player.sendPacketsToViewers(
+        if (!freshViewer) {
+            // remove the old player info and entity -- avoids nickname detection
+            target.sendPackets(
+                    new PlayerInfoRemovePacket(player.getUuid()),
+                    new DestroyEntitiesPacket(player.getEntityId())
+            );
+        }
+
+        target.sendPackets(
                 new PlayerInfoUpdatePacket(PlayerInfoUpdatePacket.Action.ADD_PLAYER, entry),
+                new SpawnEntityPacket(player.getEntityId(), player.getUuid(), EntityType.PLAYER.id(), player.getPosition(), player.getPosition().yaw(), 0, (short) 0, (short) 0, (short) 0),
                 new EntityMetaDataPacket(player.getEntityId(), Map.of(17, Metadata.Byte((byte) 127)))
         );
-        player.updateViewerRule();
+        Cytosis.getRankManager().setupCosmetics(player, data.rank());
     }
 
     @Nullable
@@ -52,7 +80,12 @@ public class NicknameManager {
         return nicknames.get(playerUuid);
     }
 
-    public record NicknameData(String nickname, @Nullable String value, @Nullable String signature) {
+    public record NicknameData(String nickname, PlayerRank rank, @Nullable String value, @Nullable String signature) {
+
+        public NicknameData(String nickname, PlayerRank rank, Tuple<String, String> data) {
+            this(nickname, rank, data.getSecond(), data.getFirst());
+        }
+
         public static NicknameData parseBytes(byte[] serialized) {
             return Cytosis.GSON.fromJson(new String(serialized), NicknameData.class);
         }
