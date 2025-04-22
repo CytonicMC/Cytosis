52276bbdacf3442975fe114dbb0cb62fae986536
foxikle <foxikle@cytonic.net>
2025-04-21 21:10:09 -0400

    Add `shouldDeanonymize` flag to ChatChannel enum
    Introduce a `shouldDeanonymize` flag to indicate if a chat channel requires de-anonymization. Updated all enum values and modified the constructor to include this new parameter. This enhances flexibility and provides better control over channel behaviors.

--- src/main/java/net/cytonic/cytosis/data/enums/ChatChannel.java
+++ src/main/java/net/cytonic/cytosis/data/enums/ChatChannel.java
@@ -12,40 +12,42 @@ public enum ChatChannel {
     /**
      * Public, single-server chat
      */
-    ALL(Component.empty()),
+    ALL(Component.empty(), false),
     /**
      * Not implementated; Private messages between two players
      */
-    PRIVATE_MESSAGE(Component.empty()),
+    PRIVATE_MESSAGE(Component.empty(), true),
     /**
      * Represents an internal chat channel used for server-specific communication.
      * This channel is not meant to be exposed to or used by regular players.
      */
-    INTERNAL_MESSAGE(Component.empty()),
+    INTERNAL_MESSAGE(Component.empty(), false),
     /**
      * Not implementated; Party chat
      */
-    PARTY(Component.text("Party > ", NamedTextColor.GOLD)),
+    PARTY(Component.text("Party > ", NamedTextColor.GOLD), false),
     /**
      * Not implementated; League chat
      */
-    LEAGUE(Component.text("League > ", NamedTextColor.DARK_PURPLE)),
+    LEAGUE(Component.text("League > ", NamedTextColor.DARK_PURPLE), true),
     /**
      * A chat channel broadcast to mods on every server
      */
-    MOD(Component.text("Mod > ", NamedTextColor.DARK_GREEN)),
+    MOD(Component.text("Mod > ", NamedTextColor.DARK_GREEN), true),
     /**
      * A chat channel broadcast to admins on every server
      */
-    ADMIN(Component.text("Admin > ", NamedTextColor.DARK_RED)),
+    ADMIN(Component.text("Admin > ", NamedTextColor.DARK_RED), true),
     /**
      * A chat channel broadcast to all staff on every server
      */
-    STAFF(Component.text("Staff > ", NamedTextColor.LIGHT_PURPLE));
+    STAFF(Component.text("Staff > ", NamedTextColor.LIGHT_PURPLE), true);
 
     private final Component prefix;
+    private final boolean shouldDeanonymize;
 
-    ChatChannel(Component prefix) {
+    ChatChannel(Component prefix, boolean shouldDeanonymize) {
         this.prefix = prefix;
+        this.shouldDeanonymize = shouldDeanonymize;
     }
 }
