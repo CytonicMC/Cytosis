69a32244a3095588d233e14aa324387bd8b4f39c
foxikle <foxikle@cytonic.net>
2025-04-26 11:44:44 -0400

    rename recalc perms command to be consistent

--- src/main/java/net/cytonic/cytosis/commands/server/RecalculatePermissionsCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/server/RecalculatePermissionsCommand.java
@@ -1,12 +1,12 @@
 package net.cytonic.cytosis.commands.server;
 
 import net.cytonic.cytosis.Cytosis;
-import net.cytonic.cytosis.commands.CytosisCommand;
+import net.cytonic.cytosis.commands.util.CytosisCommand;
 import net.cytonic.cytosis.player.CytosisPlayer;
 
-public class RecalculatePermissions extends CytosisCommand {
+public class RecalculatePermissionsCommand extends CytosisCommand {
 
-    public RecalculatePermissions() {
+    public RecalculatePermissionsCommand() {
         super("recalculatepermissions", "recalcperms");
         setDefaultExecutor((sender, context) -> {
             if (!(sender instanceof CytosisPlayer player)) return;
--- src/main/java/net/cytonic/cytosis/commands/util/CommandHandler.java
+++ src/main/java/net/cytonic/cytosis/commands/util/CommandHandler.java
@@ -66,7 +66,7 @@ public class CommandHandler {
                 new WarnCommand(),
                 new CooldownCommand(Cytosis.getNetworkCooldownManager()),
                 new LoopCommand(),
-                new RecalculatePermissions(),
+                new RecalculatePermissionsCommand(),
                 new YoinkCommand(),
                 new ReplyCommand(),
                 new SnooperCommand(),
