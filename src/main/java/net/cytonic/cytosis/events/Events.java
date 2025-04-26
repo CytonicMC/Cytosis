c4b0e44c1dda81516f02de85f0765498bdafb3f1
foxikle <foxikle@cytonic.net>
2025-04-26 11:43:49 -0400

    Add 2 events for players joining and leaving the network

--- src/main/java/net/cytonic/cytosis/events/Events.java
+++ src/main/java/net/cytonic/cytosis/events/Events.java
@@ -2,6 +2,8 @@ package net.cytonic.cytosis.events;
 
 import lombok.experimental.UtilityClass;
 import net.cytonic.cytosis.Cytosis;
+import net.cytonic.cytosis.events.network.PlayerJoinNetworkEvent;
+import net.cytonic.cytosis.events.network.PlayerLeaveNetworkEvent;
 import net.cytonic.cytosis.utils.events.PlayerJoinEventResponse;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
@@ -19,7 +21,7 @@ import java.util.function.Consumer;
  * The events contained in this class run at a priority of 50, which is fairly low as
  * event priorities go. If the event is async by nature, ie {@link AsyncPlayerConfigurationEvent},
  * then the event will be run off of ticking threads. Otherwise, the even will be run
- * on ticking threads.
+ * on ticking threads. These handlers ignore if the event has already been cancelled.
  */
 @SuppressWarnings({"unused", "unchecked"})
 @UtilityClass
@@ -32,36 +34,44 @@ public final class Events {
     private static final List<Consumer<PlayerPacketEvent>> packetInHigh = new ArrayList<>();
     private static final List<Consumer<PlayerPacketOutEvent>> packetOutLow = new ArrayList<>();
     private static final List<Consumer<PlayerPacketEvent>> packetInLow = new ArrayList<>();
+    public static final List<Consumer<PlayerLeaveNetworkEvent>> networkLeave = new ArrayList<>();
+    public static final List<Consumer<PlayerJoinNetworkEvent>> networkJoin = new ArrayList<>();
 
     static {
         Cytosis.getEventHandler().registerListeners(
                 new EventListener<>(AsyncPlayerConfigurationEvent.class,
-                        event -> spawnConsumers.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> spawnConsumers.forEach(consumer -> consumer.accept(event)),
                         false, 50, "cytosis:events_util_join", true),
                 new EventListener<>(PlayerDisconnectEvent.class,
-                        event -> disconnectConsumers.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> disconnectConsumers.forEach(consumer -> consumer.accept(event)),
                         false, 50, "cytosis:events_util_leave", true),
                 // medium prio
                 new EventListener<>(PlayerPacketEvent.class,
-                        event -> packetIn.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> packetIn.forEach(consumer -> consumer.accept(event)),
                         false, 50, "cytosis:events_util_packet_in", true),
                 new EventListener<>(PlayerPacketOutEvent.class,
-                        event -> packetOut.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> packetOut.forEach(consumer -> consumer.accept(event)),
                         false, 50, "cytosis:events_util_packet_out", true),
                 // low prio
                 new EventListener<>(PlayerPacketEvent.class,
-                        event -> packetInLow.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> packetInLow.forEach(consumer -> consumer.accept(event)),
                         false, 100, "cytosis:events_util_packet_in_low", true),
                 new EventListener<>(PlayerPacketOutEvent.class,
-                        event -> packetOutLow.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> packetOutLow.forEach(consumer -> consumer.accept(event)),
                         false, 100, "cytosis:events_util_packet_out_low", true),
                 // high prio
                 new EventListener<>(PlayerPacketEvent.class,
-                        event -> packetInHigh.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
+                        event -> packetInHigh.forEach(consumer -> consumer.accept(event)),
                         false, 0, "cytosis:events_util_packet_in_high", true),
                 new EventListener<>(PlayerPacketOutEvent.class,
-                        event -> packetOutHigh.forEach(eventListenerConsumer -> eventListenerConsumer.accept(event)),
-                        false, 0, "cytosis:events_util_packet_out_high", true)
+                        event -> packetOutHigh.forEach(consumer -> consumer.accept(event)),
+                        false, 0, "cytosis:events_util_packet_out_high", true),
+                new EventListener<>(PlayerJoinNetworkEvent.class,
+                        event -> networkJoin.forEach(consumer -> consumer.accept(event)),
+                        false, 0, "cytosis:events_util_network_join", true),
+                new EventListener<>(PlayerLeaveNetworkEvent.class,
+                        event -> networkLeave.forEach(consumer -> consumer.accept(event)),
+                        false, 0, "cytosis:events_util_network_leave", true)
         );
     }
 
@@ -177,5 +187,28 @@ public final class Events {
     public static void onPacketOutLowPriority(Consumer<PlayerPacketOutEvent> event) {
         packetOutLow.add(event);
     }
+
+
+    /**
+     * Registers a consumer that will be executed when a player joins the network.
+     * This method adds the given consumer to the internal handler for network join events.
+     *
+     * @param event The consumer that processes the player join network event.
+     */
+    public static void onNetworkJoin(Consumer<PlayerJoinNetworkEvent> event) {
+        networkJoin.add(event);
+    }
+
+
+    /**
+     * Registers a consumer that will be executed when a player leaves the network.
+     * This method adds the given consumer to the internal handler for network leave events.
+     *
+     * @param event The consumer that processes the player leave network event.
+     */
+    public static void onNetworkLeave(Consumer<PlayerLeaveNetworkEvent> event) {
+        networkLeave.add(event);
+    }
+
 
 }
