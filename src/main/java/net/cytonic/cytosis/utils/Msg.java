f33a4d8d20b6a804c48d9489e2d59c9de61d703e
foxikle <foxikle@cytonic.net>
2025-04-27 08:05:30 -0400

    Fix duplicating # in hex strings

--- src/main/java/net/cytonic/cytosis/utils/Msg.java
+++ src/main/java/net/cytonic/cytosis/utils/Msg.java
@@ -134,11 +134,11 @@ public interface Msg {
     }
 
     static Component badge(String splash, String hex) {
-        return mm("<#" + hex + "><b>" + splash + "</b></#" + hex + ">");
+        return mm("<#" + hex.replace("#", "") + "><b>" + splash + "</b></#" + hex.replace("#", "") + ">");
     }
 
     static Component badge(String splash, TextColor color) {
-        return mm("<#" + color.asHexString() + "><b>" + splash + "</b></#" + color.asHexString() + ">");
+        return mm("<" + color.asHexString() + "><b>" + splash + "</b></" + color.asHexString() + ">");
     }
 
     static Component aqua(String text, Object... args) {
