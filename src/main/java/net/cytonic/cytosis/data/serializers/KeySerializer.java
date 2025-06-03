cf052cbe3ec0869abd2f6742ca9efedfa2a993e4
webhead1104 <webhead1104@hotmail.com>
2025-06-03 18:54:23 -0500

    Add Cytosis.GSON_CONFIGURATION_LOADER, KeySerializer, and PosSerializer

---
+++ src/main/java/net/cytonic/cytosis/data/serializers/KeySerializer.java
@@ -0,0 +1,27 @@
+package net.cytonic.cytosis.data.serializers;
+
+import net.kyori.adventure.key.Key;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.serialize.SerializationException;
+import org.spongepowered.configurate.serialize.TypeSerializer;
+
+import java.lang.reflect.Type;
+
+public class KeySerializer implements TypeSerializer<Key> {
+
+    @Override
+    public Key deserialize(@NotNull Type type, @NotNull ConfigurationNode node) throws SerializationException {
+        String key = node.getString();
+        if (key == null) throw new SerializationException("Cannot deserialize a null key!");
+        return Key.key(key);
+    }
+
+
+    @Override
+    public void serialize(@NotNull Type type, @Nullable Key obj, @NotNull ConfigurationNode node) throws SerializationException {
+        if (obj == null) throw new SerializationException("Cannot serialize a null key!");
+        node.set(obj.asString());
+    }
+}
