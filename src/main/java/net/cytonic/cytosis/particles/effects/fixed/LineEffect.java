89a384a260a6cf877a1a1fcbc37157d7b246fd5a
foxikle <foxikle@cytonic.net>
2025-07-07 14:24:26 -0400

    Suppor double densities, and Point

--- src/main/java/net/cytonic/cytosis/particles/effects/fixed/LineEffect.java
+++ src/main/java/net/cytonic/cytosis/particles/effects/fixed/LineEffect.java
@@ -3,18 +3,18 @@ package net.cytonic.cytosis.particles.effects.fixed;
 import net.cytonic.cytosis.particles.util.ParticleSupplier;
 import net.cytonic.cytosis.utils.Utils;
 import net.minestom.server.adventure.audience.PacketGroupingAudience;
-import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Vec;
 
 import java.util.List;
 
 public class LineEffect extends StaticEffect {
-    Pos pos1, pos2;
-    int density;
+    Point pos1, pos2;
+    double density;
     ParticleSupplier supplier;
-    List<Pos> positions;
+    List<Point> positions;
 
-    public LineEffect(ParticleSupplier supplier, Pos pos1, Pos pos2, int density) {
+    public LineEffect(ParticleSupplier supplier, Point pos1, Point pos2, double density) {
         this.supplier = supplier;
         this.pos1 = pos1;
         this.pos2 = pos2;
@@ -22,12 +22,12 @@ public class LineEffect extends StaticEffect {
         this.positions = computePositions();
     }
 
-    private List<Pos> computePositions() {
+    private List<Point> computePositions() {
         double dist = pos1.distance(pos2);
         int steps = (int) (density * dist);
         double increment = dist / steps;
-        List<Pos> positions = Utils.list(pos1);
-        Vec normalized = pos2.sub(pos1).asVec().normalize();
+        List<Point> positions = Utils.list(pos1);
+        Vec normalized = Vec.fromPoint(pos2.sub(pos1)).normalize();
         for (int i = 1; i < steps; i++) {
             double t = i * increment;
             positions.add(pos1.add(normalized.mul(t)));
