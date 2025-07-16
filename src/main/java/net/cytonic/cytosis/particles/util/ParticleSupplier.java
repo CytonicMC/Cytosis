2849193d01102a41db25cac5137a32513fc17c54
foxikle <foxikle@cytonic.net>
2025-07-16 10:50:40 -0400

    Random particle supplier

--- src/main/java/net/cytonic/cytosis/particles/util/ParticleSupplier.java
+++ src/main/java/net/cytonic/cytosis/particles/util/ParticleSupplier.java
@@ -3,4 +3,8 @@ package net.cytonic.cytosis.particles.util;
 @FunctionalInterface
 public interface ParticleSupplier {
     ParticleData get();
+
+    static ParticleSupplier random(ParticleData... data) {
+        return () -> data[(int) (Math.random() * data.length)];
+    }
 }
