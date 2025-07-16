c13055910882577ef13e0a98a3b8fbbc88d6a7ce
foxikle <foxikle@cytonic.net>
2025-07-16 10:53:51 -0400

    Fix aoobe if 0 elements are supplied

--- src/main/java/net/cytonic/cytosis/particles/util/ParticleSupplier.java
+++ src/main/java/net/cytonic/cytosis/particles/util/ParticleSupplier.java
@@ -5,6 +5,9 @@ public interface ParticleSupplier {
     ParticleData get();
 
     static ParticleSupplier random(ParticleData... data) {
+        if (data.length == 0) {
+            throw new IllegalArgumentException("At least one particle data is required");
+        }
         return () -> data[(int) (Math.random() * data.length)];
     }
 }
