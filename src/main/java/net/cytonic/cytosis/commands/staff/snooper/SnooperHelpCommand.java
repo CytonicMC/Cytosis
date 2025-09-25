543661813865b053b6a9e80ba34e18ba15f5b679
foxikle <foxikle@cytonic.net>
2025-09-24 20:02:23 -0400

    fix stackoverflow with running /snooper help

--- src/main/java/net/cytonic/cytosis/commands/staff/snooper/SnooperHelpCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/staff/snooper/SnooperHelpCommand.java
@@ -7,11 +7,28 @@ import net.cytonic.cytosis.commands.staff.snooper.help.SnooperHelpListen;
 import net.cytonic.cytosis.commands.staff.snooper.help.SnooperHelpMute;
 import net.cytonic.cytosis.commands.staff.snooper.help.SnooperHelpUnmute;
 import net.cytonic.cytosis.commands.utils.CytosisCommand;
+import net.cytonic.cytosis.utils.Msg;
 
 public class SnooperHelpCommand extends CytosisCommand {
 
     public SnooperHelpCommand() {
         super("help");
+
+        setDefaultExecutor((sender, ignored) -> sender.sendMessage(
+            Msg.splash("SNOOPER HELP!", "e829aa", "»").appendNewline().append(Msg.grey(
+                """
+                    /snooper <#ee61c0>about</#ee61c0>: Gives information about what snooper is, its\
+                     limitations, and advantages.
+                    /snooper <#ee61c0>help</#ee61c0>: Displays this message.
+                    /snooper <#ee61c0>listen</#ee61c0> <channel>: Starts snooping on a specific channel.
+                    /snooper <#ee61c0>mute</#ee61c0>: Completely disables receiving snoops. <i><dark_gray>This is required\
+                     to be enabled in any public videos or screen recordings.</i>
+                    /snooper <#ee61c0>unmute</#ee61c0>: Re-enables receiving snoops.
+                    /snooper <#ee61c0>audit</#ee61c0> <channel>: Opens the GUI to "audit" past snoops, allowing you to\
+                    view all previously sent snoops on a given channel.
+                    /snooper <#ee61c0>blind</#ee61c0> <channel>: Stops snooping on a specific channel.
+                    """))));
+
         addSubcommand(new SnooperHelpAbout());
         addSubcommand(new SnooperHelpAudit());
         addSubcommand(new SnooperHelpBlind());
