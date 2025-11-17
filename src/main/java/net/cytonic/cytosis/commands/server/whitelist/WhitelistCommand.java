e9e1148297a7d32a25a9245e8f04f4a0ea4fe977
foxikle <foxikle@cytonic.net>
2025-11-16 19:30:19 -0500

    Fix entrypoint

---
+++ src/main/java/net/cytonic/cytosis/commands/server/whitelist/WhitelistCommand.java
@@ -0,0 +1,28 @@
+package net.cytonic.cytosis.commands.server.whitelist;
+
+import net.kyori.adventure.text.Component;
+
+import net.cytonic.cytosis.commands.utils.CommandUtils;
+import net.cytonic.cytosis.commands.utils.CytosisCommand;
+import net.cytonic.cytosis.utils.Msg;
+
+public class WhitelistCommand extends CytosisCommand {
+
+    private static final Component HELP = Msg.mm("""
+        <aqua><bold>Server Whitelist</bold></aqua>
+        
+        Server whitelists allow non-staff players to join the <red>Alpha</red> and <#ab2bfb>Development</#ab2bfb> \
+        networks. <gray><i>Whitelists do not override bans.</i></gray>
+        
+        Usages:
+        - /whitelist grant <player> [network] <i><gray>The network defaults to the current environment</gray></i>
+        - /whitelist revoke <player>
+        - /whitelist status <player>
+        """);
+
+    public WhitelistCommand() {
+        super("whitelist");
+        setCondition(CommandUtils.IS_STAFF);
+        setDefaultExecutor((sender, _) -> sender.sendMessage(HELP));
+    }
+}
