e978b8df62173a03f4558fcea342382392c665c2
Webhead1104 <webhead1104@hotmail.com>
2026-01-11 20:25:07 -0600

    Hide debug behind system property nats_debug

--- src/main/java/net/cytonic/cytosis/protocol/NatsAPIImpl.java
+++ src/main/java/net/cytonic/cytosis/protocol/NatsAPIImpl.java
@@ -4,7 +4,6 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import io.nats.client.Message;
-import lombok.extern.slf4j.Slf4j;
 
 import net.cytonic.cytosis.Bootstrappable;
 import net.cytonic.cytosis.Cytosis;
@@ -14,38 +13,41 @@ import net.cytonic.cytosis.messaging.NatsManager;
 import net.cytonic.protocol.ProtocolHelper;
 import net.cytonic.protocol.utils.NatsAPI;
 
-@Slf4j
 @CytosisComponent(dependsOn = NatsManager.class)
 public class NatsAPIImpl implements NatsAPI, Bootstrappable {
 
     @Override
     public void init() {
-        Logger.debug("Initializing Nats API");
         ProtocolHelper.init();
-        Logger.debug("After");
     }
 
     @Override
     public void subscribe(String channel, Consumer<Message> consumer) {
-        log.info("Subscribing to channel {}", channel);
+        log("Subscribing to channel %s", channel);
         Cytosis.get(NatsManager.class).subscribe(channel, message -> {
-            log.info("Received Nats Message from sub {} {}", channel, new String(message.getData()));
+            log("Received Nats Message from sub %s %s", channel, new String(message.getData()));
             consumer.accept(message);
         });
     }
 
     @Override
     public void publish(String channel, String data) {
-        log.info("Publishing to channel {} {}", channel, data);
+        log("Publishing to channel %s %s", channel, data);
         Cytosis.get(NatsManager.class).publish(channel, data.getBytes());
     }
 
     @Override
     public void request(String channel, String data, BiConsumer<byte[], Throwable> consumer) {
-        log.info("Requesting on channel {} {}", channel, data);
+        log("Requesting on channel %s %s", channel, data);
         Cytosis.get(NatsManager.class).request(channel, data.getBytes(), ((message, throwable) -> {
-            log.info("Received Nats request from request {} {}", channel, new String(message.getData()));
+            log("Received Nats request from request %s %s", channel, new String(message.getData()));
             consumer.accept(message.getData(), throwable);
         }));
+    }
+
+    private static void log(String message, Object... args) {
+        if (Boolean.getBoolean("nats_debug")) {
+            Logger.debug(message, args);
+        }
     }
 }
