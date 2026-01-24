2a3a9a4e412a85c667a3e6a95d39f4afcd5aa6da
foxikle <foxikle@cytonic.net>
2026-01-24 13:23:31 -0500

    Add messages to the tps command

--- src/main/java/net/cytonic/cytosis/commands/server/TpsCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/server/TpsCommand.java
@@ -3,29 +3,30 @@ package net.cytonic.cytosis.commands.server;
 import java.util.Set;
 
 import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.bossbar.BossBar.Overlay;
 import net.kyori.adventure.text.Component;
 
 import net.cytonic.cytosis.commands.utils.CytosisCommand;
-import net.cytonic.cytosis.events.ServerEventListeners;
 import net.cytonic.cytosis.player.CytosisPlayer;
-import net.cytonic.cytosis.utils.CytosisPreferences;
+import net.cytonic.cytosis.utils.Msg;
+import net.cytonic.cytosis.utils.Preferences;
 
 public class TpsCommand extends CytosisCommand {
 
     public static final BossBar BAR = BossBar.bossBar(Component.empty(), 1, BossBar.Color.BLUE,
-        BossBar.Overlay.NOTCHED_20, Set.of());
+        Overlay.PROGRESS, Set.of());
 
     public TpsCommand() {
         super("tps");
-        setDefaultExecutor((sender, context) -> {
+        setDefaultExecutor((sender, _) -> {
             if (!(sender instanceof CytosisPlayer player)) return;
-            boolean newVal = player.getPreference(CytosisPreferences.TPS_DEBUG);
-            player.updatePreference(CytosisPreferences.TPS_DEBUG, !newVal);
+            boolean newVal = !player.getPreference(Preferences.TPS_DEBUG);
+            player.updatePreference(Preferences.TPS_DEBUG, newVal);
             if (newVal) {
-                ServerEventListeners.TPS_CACHE.add(player.getUuid());
+                player.sendMessage(Msg.success("Showing server performance metrics!"));
                 player.showBossBar(BAR);
             } else {
-                ServerEventListeners.TPS_CACHE.remove(player.getUuid());
+                player.sendMessage(Msg.success("Hiding server performance metrics!"));
                 player.scheduleNextTick(_ -> player.hideBossBar(BAR));
             }
         });
