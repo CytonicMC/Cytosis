316c0f05c3b9ecca4bbb82b1bd5bd1fbf92271ff
foxikle <foxikle@cytonic.net>
2026-01-25 08:56:24 -0500

    Clean up /server command

--- src/main/java/net/cytonic/cytosis/commands/server/ServerCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/server/ServerCommand.java
@@ -2,8 +2,6 @@ package net.cytonic.cytosis.commands.server;
 
 import java.util.List;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.ArgumentWord;
 import net.minestom.server.command.builder.suggestion.SuggestionEntry;
@@ -13,9 +11,6 @@ import net.cytonic.cytosis.Cytosis;
 import net.cytonic.cytosis.CytosisContext;
 import net.cytonic.cytosis.commands.utils.CommandUtils;
 import net.cytonic.cytosis.commands.utils.CytosisCommand;
-import net.cytonic.cytosis.data.objects.CytonicServer;
-import net.cytonic.cytosis.protocol.publishers.SendPlayerToServerPacketPublisher;
-import net.cytonic.cytosis.logging.Logger;
 import net.cytonic.cytosis.player.CytosisPlayer;
 import net.cytonic.cytosis.utils.Msg;
 
@@ -29,39 +24,25 @@ public class ServerCommand extends CytosisCommand {
      */
     public ServerCommand() {
         super("cytosis:server", "server");
-        try {
-            setCondition(CommandUtils.IS_STAFF);
-            setDefaultExecutor((sender, cmdc) -> sender.sendMessage(Msg.mm("<RED>You must specify a server!")));
-            ArgumentWord serverArgument = ArgumentType.Word("server");
-            serverArgument.setCallback((sender, exception) -> sender.sendMessage(
-                Component.text("The server " + exception.getInput() + " is invalid!", NamedTextColor.RED)));
-            serverArgument.setSuggestionCallback((cmds, cmdc, suggestion) -> {
-                List<SuggestionEntry> options = Cytosis.get(CytonicNetwork.class).getServers().values()
-                    .stream()
-                    .map(cytonicServer -> new SuggestionEntry(cytonicServer.id()))
-                    .toList();
-                CommandUtils.filterEntries(cmdc.get(serverArgument), options).forEach(suggestion::addEntry);
-            });
-            addSyntax(((sender, context) -> {
-                if (sender instanceof CytosisPlayer player) {
-                    if (!player.isStaff()) return;
-                    if (!context.get(serverArgument).equalsIgnoreCase(CytosisContext.SERVER_ID)) {
-                        for (CytonicServer server : Cytosis.get(CytonicNetwork.class).getServers()
-                            .values()) {
-                            if (server.id().equals(context.get(serverArgument))) {
-                                player.sendMessage(
-                                    Component.text("Connecting to " + server.id(), NamedTextColor.GREEN));
-                                Cytosis.get(SendPlayerToServerPacketPublisher.class)
-                                    .sendPlayerToServer(player.getUuid(), server);
-                            }
-                        }
-                    } else {
-                        player.sendMessage(Msg.mm("<RED>You are already connected to the server!"));
-                    }
-                }
-            }), serverArgument);
-        } catch (Exception e) {
-            Logger.error("error", e);
-        }
+        setCondition(CommandUtils.IS_STAFF);
+        setDefaultExecutor((sender, _) -> sender.sendMessage(Msg.whoops("You must specify a server!")));
+        ArgumentWord serverArgument = ArgumentType.Word("server");
+        serverArgument.setSuggestionCallback((_, ctx, suggestion) -> {
+            List<SuggestionEntry> options = Cytosis.get(CytonicNetwork.class).getServers().keySet()
+                .stream()
+                .filter(s -> !s.equalsIgnoreCase(CytosisContext.SERVER_ID))
+                .map(SuggestionEntry::new)
+                .toList();
+            CommandUtils.filterEntries(ctx.get(serverArgument), options).forEach(suggestion::addEntry);
+        });
+        addSyntax(((sender, context) -> {
+            if (!(sender instanceof CytosisPlayer player)) return;
+            if (!player.isStaff()) return;
+            if (context.get(serverArgument).equalsIgnoreCase(CytosisContext.SERVER_ID)) {
+                player.sendMessage(Msg.whoops("You are already connected to %s!", CytosisContext.SERVER_ID));
+                return;
+            }
+            player.sendToServer(context.get(serverArgument));
+        }), serverArgument);
     }
 }
