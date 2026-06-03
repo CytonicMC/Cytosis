118f64f6bbfbd64bc9c56c6f72d8e61e469c265e
Webhead1104 <webhead1104@hotmail.com>
2026-06-03 14:11:34 -0500

    fix: register instance on create

--- src/main/java/net/cytonic/cytosis/world/AbstractWorld.java
+++ src/main/java/net/cytonic/cytosis/world/AbstractWorld.java
@@ -1,12 +1,16 @@
 package net.cytonic.cytosis.world;
 
 import java.util.UUID;
+
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.registry.RegistryKey;
 import net.minestom.server.world.DimensionType;
 
 public abstract class AbstractWorld extends InstanceContainer {
+
     public AbstractWorld(UUID uuid, RegistryKey<DimensionType> dimensionType) {
         super(uuid, dimensionType);
+        MinecraftServer.getInstanceManager().registerInstance(this);
     }
 }
