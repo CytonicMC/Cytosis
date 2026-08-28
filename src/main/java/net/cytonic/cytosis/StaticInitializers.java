7989a61ddef77778c4d2d2cc3753f1e720419063
foxikle <foxikle@cytonic.net>
2026-08-28 09:45:45 -0400

    update static initializers to hold reference to the server

--- src/main/java/net/cytonic/cytosis/StaticInitializers.java
+++ src/main/java/net/cytonic/cytosis/StaticInitializers.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import net.minestom.server.Auth;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Player;
 import net.minestom.server.instance.DynamicChunk;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.anvil.AnvilLoader;
 
+import net.cytonic.cytosis.player.CytosisPlayer;
+
 public class StaticInitializers {
+
+    public static final MinecraftServer SERVER;
 
     static {
         // set some server flags
@@ -18,10 +21,12 @@ public class StaticInitializers {
         System.setProperty("minestom.chunk-view-distance", "12");
         System.setProperty("event.multiple-parents", "true");
 
+
         // preinitialize the minecraft server :)
-        MinecraftServer.init(new Auth.Velocity("TESTSECRET"));
+        SERVER = MinecraftServer.init(new Auth.Velocity("TESTSECRET"));
 
         // forces initialization order
-        var ignoredTheClasses = List.of(AnvilLoader.class, DynamicChunk.class, Player.class, InstanceContainer.class);
+        var ignoredTheClasses = List.of(AnvilLoader.class, DynamicChunk.class, CytosisPlayer.class,
+            InstanceContainer.class);
     }
 }
