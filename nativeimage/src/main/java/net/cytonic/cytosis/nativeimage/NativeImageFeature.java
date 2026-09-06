86240db7c1f31e70269fefe7a54c117c237864f1
Foxikle <foxikle@cytonic.net>
2026-09-06 14:32:16 -0400

    Fix: Minio classes getting mangled by the native image compiler

--- nativeimage/src/main/java/net/cytonic/cytosis/nativeimage/NativeImageFeature.java
+++ nativeimage/src/main/java/net/cytonic/cytosis/nativeimage/NativeImageFeature.java
@@ -9,6 +9,8 @@ import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
+import java.lang.reflect.Constructor;
+
 /// Stolen from
 /// [hollow-cube/mapmaker](https://github.com/hollow-cube/mapmaker/blob/main/tools/native-image-helper/src/main/java/net/hollowcube/nativeimage/HCNativeImageFeature.java)
 /// with some modifications, mostly removing their proprietary stuff
@@ -21,16 +23,16 @@ public class NativeImageFeature implements Feature {
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
         try (ScanResult scanResult = new ClassGraph()
-            .overrideClasspath(access.getApplicationClassPath())
-            .enableClassInfo()
-            .enableFieldInfo()
-            .enableAnnotationInfo()
-            .enableMethodInfo()
-            .ignoreClassVisibility()
-            .ignoreMethodVisibility()
-            .ignoreFieldVisibility()
-            .acceptPackages("net.cytonic", "net.minestom")
-            .scan()) {
+                .overrideClasspath(access.getApplicationClassPath())
+                .enableClassInfo()
+                .enableFieldInfo()
+                .enableAnnotationInfo()
+                .enableMethodInfo()
+                .ignoreClassVisibility()
+                .ignoreMethodVisibility()
+                .ignoreFieldVisibility()
+                .acceptPackages("net.cytonic", "net.minestom")
+                .scan()) {
 
             scanResult.getSubclasses(Record.class).forEach(ci -> processRecordClass(access, ci));
 
@@ -46,19 +48,57 @@ public class NativeImageFeature implements Feature {
             processScan(scanResult.getSubclasses("net.cytonic.cytosis.commands.utils.CytosisCommand"), access);
             processScan(scanResult.getSubclasses("net.cytonic.cytosis.entity.npc.NPC"), access);
             processScan(
-                scanResult.getClassesWithAnnotation("net.cytonic.cytosis.bootstrap.annotations.CytosisComponent"),
-                access);
+                    scanResult.getClassesWithAnnotation("net.cytonic.cytosis.bootstrap.annotations.CytosisComponent"),
+                    access);
 
             processMinestomMetadataDef(access);
         }
 
         try (ScanResult scanResult = new ClassGraph()
-            .overrideClasspath(access.getApplicationClassPath())
-            .enableClassInfo()
-            .ignoreClassVisibility()
-            .acceptPackages("net.minestom", "net.kyori", "ch.qos.logback", "org.jctools", "it.unimi.dsi.fastutil",
-                "com.google.gson", "io.nats.client")
-            .scan()) {
+                .overrideClasspath(access.getApplicationClassPath())
+                .enableClassInfo()
+                .enableFieldInfo()
+                .enableAnnotationInfo()
+                .enableMethodInfo()
+                .ignoreClassVisibility()
+                .ignoreMethodVisibility()
+                .ignoreFieldVisibility()
+                .acceptPackages("io.minio")
+                .scan()) {
+            scanResult.getSubclasses("io.minio.BaseArgs").forEach(ci -> {
+                Class<?> clazz = access.findClassByName(ci.getName());
+
+                if (clazz == null) {
+                    IO.println("Could not resolve " + ci.getName());
+                    return;
+                }
+
+                IO.println("Registering MinIO reflection metadata for " + clazz.getName());
+
+                RuntimeReflection.register(clazz);
+                RuntimeReflection.registerClassLookup(clazz.getName());
+
+                // Explicitly preserve the no-arg constructor.
+                try {
+                    Constructor<?> ctor = clazz.getDeclaredConstructor();
+                    RuntimeReflection.register(ctor);
+                } catch (NoSuchMethodException ignored) {
+                    System.err.println("No no-arg constructor on " + clazz.getName());
+                }
+
+                // Optional: preserve every constructor as well.
+                RuntimeReflection.registerAllConstructors(clazz);
+            });
+        }
+
+
+        try (ScanResult scanResult = new ClassGraph()
+                .overrideClasspath(access.getApplicationClassPath())
+                .enableClassInfo()
+                .ignoreClassVisibility()
+                .acceptPackages("net.minestom", "net.kyori", "ch.qos.logback", "org.jctools", "it.unimi.dsi.fastutil",
+                        "com.google.gson", "io.nats.client")
+                .scan()) {
 
             for (PackageInfo packageInfo : scanResult.getPackageInfo()) {
                 if ("net.minestom.server.utils".equals(packageInfo.getName()))
@@ -70,27 +110,27 @@ public class NativeImageFeature implements Feature {
             }
 
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.network.NetworkBufferImpl"));
+                    access.findClassByName("net.minestom.server.network.NetworkBufferImpl"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.network.NetworkBufferImpl$CompressionHolder"));
+                    access.findClassByName("net.minestom.server.network.NetworkBufferImpl$CompressionHolder"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.utils.PacketViewableUtils$ViewableStorage"));
+                    access.findClassByName("net.minestom.server.utils.PacketViewableUtils$ViewableStorage"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.utils.ObjectPool"));
+                    access.findClassByName("net.minestom.server.utils.ObjectPool"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.network.player.PlayerSocketConnection"));
+                    access.findClassByName("net.minestom.server.network.player.PlayerSocketConnection"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.network.packet.PacketVanilla"));
+                    access.findClassByName("net.minestom.server.network.packet.PacketVanilla"));
 
             // LoginListener holds a static SecureRandom. GraalVM disallows Random/SecureRandom instances in the
             // image heap entirely, so it must not be initialized at build time by the blanket net.minestom init.
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("net.minestom.server.listener.preplay.LoginListener"));
+                    access.findClassByName("net.minestom.server.listener.preplay.LoginListener"));
 
             // https://github.com/nats-io/nats.java#integration-with-graalvm
             RuntimeClassInitialization.initializeAtRunTime(access.findClassByName("java.security.SecureRandom"));
             RuntimeClassInitialization.initializeAtRunTime(
-                access.findClassByName("io.nats.client.support.RandomUtils"));
+                    access.findClassByName("io.nats.client.support.RandomUtils"));
             RuntimeClassInitialization.initializeAtRunTime(access.findClassByName("io.nats.client.NUID"));
         }
 
