259e2e677358e3101c0de8a7073ca6cb4a929126
Zffu <103074097+Zffu@users.noreply.github.com>
2026-09-07 03:22:20 +0200

    feat: added sidebar viewers

--- src/main/java/net/cytonic/cytosis/sidebar/SidebarComponent.java
+++ src/main/java/net/cytonic/cytosis/sidebar/SidebarComponent.java
@@ -1,7 +1,8 @@
 package net.cytonic.cytosis.sidebar;
 
-import net.cytonic.cytosis.player.CytosisPlayer;
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
@@ -14,26 +15,30 @@ import java.util.Collection;
  * - A component can be either updated by a scheduler or manually updated when the contents change.
  * - A component can be shown or hidden to individual players.
  * - A component is part of a sidebar.
- * @param <P> The player type
+ * @param <V> The viewer type
+ *
+ * This should be instantiated once per "component" and stored somewhere.
  */
-public interface SidebarComponent<P extends CytosisPlayer> {
+public interface SidebarComponent<V extends SidebarViewer<V>> {
 
     /**
-     * Checks if the given player can display the component.
-     * This is only checked whether the component is about to be displayed to player. Not on each update.
-     * Meaning, you can use {@link #canDisplay} to determine if a part of a scoreboard will be shown to the player.
+     * Checks if the given viewer can display the component.
+     * This is only checked whether the component is about to be displayed to viewer. Not on each update.
+     * Meaning, you can use {@link #canDisplay} to determine if a part of a scoreboard will be shown to the viewer.
      *
-     * @param player The player.
+     * @param viewer The viewer.
      */
-    boolean canDisplay(P player);
+    boolean canDisplay(@NotNull V viewer);
 
     /**
-     * Builds the contents of the sidebar component based on the player.
+     * Builds the contents of the sidebar component based on the viewer.
      * This will output a collection of length {@link #getComponentLength()} and that should be assumed.
+     * The output collection can be null if the viewer isn't supposed to view the component, whether it is null or not in this case
+     * is defined by how the component implementation of {@link #getContents(SidebarViewer)} is created.
      *
-     * @param player The player.
+     * @param viewer The viewer.
      */
-    Collection<Component> getContents(P player);
+    @Nullable Collection<Component> getContents(@NotNull V viewer);
 
     /**
      * Get the length of the component in lines.
---
+++ src/main/java/net/cytonic/cytosis/sidebar/SidebarViewer.java
@@ -0,0 +1,69 @@
+package net.cytonic.cytosis.sidebar;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a viewer of a sidebar.
+ *
+ * @param <V> the self viewer type.
+ */
+public interface SidebarViewer<V extends SidebarViewer<V>> {
+
+    /**
+     * Checks if the viewer is currently viewing the component.
+     * This doesn't check if the viewer <b>can</b> view the component, just if the viewer is currently viewing it.
+     * @param component The component.
+     */
+    void isViewingComponent(@NotNull SidebarComponent<V> component);
+
+    /**
+     * Display the component to the viewer if the viewer can view it at given offset.
+     * This generally cannot fail due to the check to make sure that the viewer is supposed to be able to view it.
+     * However, if you wish to display this component anyway, use {@link #forceDisplayComponent(SidebarComponent, int)}.
+     * <p>
+     * If the component is already being displayed, the function will fail and return false.
+     * <p>
+     * <h1>Update warnings</h1>
+     * <p>This function <b>will</b> update the viewer's scoreboard if it doesn't fail.</p>
+     * <p>Adding at the end of the scoreboard is usually the fastest scenario since no lines need to be moved.</p>
+     * <p>This function may move other lines if needed using a `set_score` packet.</p>
+     *
+     * @param component The component to display.
+     * @param offset The line offset where to display the component. -1 is at the end of the scoreboard (the top) and 0 at the start (the bottom).
+     * @return the fail state of the display. If the component couldn't be displayed, return false, otherwise true.
+     */
+    boolean displayComponent(@NotNull SidebarComponent<V> component, int offset);
+
+    /**
+     * The "unsafe" variant of {@link #displayComponent(SidebarComponent, int)}.
+     * Displays the component to viewer regardless of whether the viewer can view it or not at the given offset.
+     * <p>
+     * If the component is already being displayed, the function will fail and return false.
+     * <p>
+     * <h1>Update warnings</h1>
+     * <p>This function <b>will</b> update the viewer's scoreboard if it doesn't fail.</p>
+     * <p>Adding at the end of the scoreboard is usually the fastest scenario since no lines need to be moved.</p>
+     * <p>This function may move other lines if needed using a `set_score` packet.</p>
+     *
+     * @param component The component to display.
+     * @param offset The line offset where to display the component. -1 is at the end of the scoreboard (the top) and 0 at the start (the bottom).
+     * @return the fail state of the display. If the component couldn't be displayed, return false, otherwise true.
+     */
+    boolean forceDisplayComponent(@NotNull SidebarComponent<V> component, int offset);
+
+    /**
+     * Hides the given component to the viewer.
+     * <p>
+     * If the component is already hidden / not being displayed, the function will fail and return false.
+     * <p>
+     * <h1>Update warnings</h1>
+     * <p>This function <b>will</b> update the viewer's scoreboard if it doesn't fail.</p>
+     * <p>This function usually is pretty fast as it only requires resetting scores rather than moving them.</p>
+     *
+     *
+     * @param component The component to hide.
+     * @return the fail state of the hide. If the component couldn't be hidden, return false, otherwise true.
+     */
+    boolean hideComponent(@NotNull SidebarComponent<V> component);
+
+}
