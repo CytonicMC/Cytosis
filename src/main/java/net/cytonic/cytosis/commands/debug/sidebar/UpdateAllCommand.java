3f954ee64ef2dbdf24f0940065ab62caf4d8a003
Zffu <103074097+Zffu@users.noreply.github.com>
2026-09-13 03:59:59 +0200

    feat: added /sidebartest update & updateall

---
+++ src/main/java/net/cytonic/cytosis/commands/debug/sidebar/UpdateAllCommand.java
@@ -0,0 +1,29 @@
+package net.cytonic.cytosis.commands.debug.sidebar;
+
+import net.cytonic.cytosis.commands.utils.CytosisCommand;
+import net.cytonic.cytosis.commands.utils.SubCommand;
+import net.cytonic.cytosis.player.CytosisPlayer;
+import net.cytonic.cytosis.sidebar.SidebarComponent;
+
+@SubCommand(SidebarTestCommand.class)
+public class UpdateAllCommand extends CytosisCommand {
+
+    public UpdateAllCommand() {
+        super("updateall");
+
+        this.setDefaultExecutor((sender, ctx) -> {
+            CytosisPlayer player = (CytosisPlayer) sender;
+
+            player.sendMessage("Updating every updatable component...");
+
+            for(SidebarComponent<CytosisPlayer> component : player.getCurrentSidebar().getComponents()) {
+                if(!component.isFullyStatic()) {
+                    player.sendMessage("Updating component " + component.getId());
+
+                    player.updateComponent(component);
+                }
+            }
+        });
+    }
+
+}
---
+++ src/main/java/net/cytonic/cytosis/commands/debug/sidebar/UpdateCommand.java
@@ -0,0 +1,45 @@
+package net.cytonic.cytosis.commands.debug.sidebar;
+
+import net.cytonic.cytosis.commands.utils.CytosisCommand;
+import net.cytonic.cytosis.commands.utils.SubCommand;
+import net.cytonic.cytosis.player.CytosisPlayer;
+import net.cytonic.cytosis.sidebar.SidebarComponent;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.suggestion.SuggestionEntry;
+
+@SubCommand(SidebarTestCommand.class)
+public class UpdateCommand extends CytosisCommand {
+
+    public UpdateCommand() {
+        super("update");
+
+        this.setDefaultExecutor((sender, ctx) -> sender.sendMessage("§cYou must specify a component ID to update! Use /sidebartest updateall to update all updatable!"));
+
+        var componentIDArgument = ArgumentType.Word("componentID").setSuggestionCallback((sender, ctx, suggestion) -> {
+            if(!(sender instanceof CytosisPlayer player)) return;
+
+            if(player.getCurrentSidebar() == null) return;
+
+            for(SidebarComponent<CytosisPlayer> component : player.getCurrentSidebar().getComponents()) {
+                if(player.isViewingComponent(component) && component.getId() != null) {
+                    suggestion.addEntry(new SuggestionEntry(component.getId()));
+                }
+            }
+        });
+
+        this.addSyntax((sender, ctx) -> {
+            CytosisPlayer player = (CytosisPlayer) sender;
+
+            String id = ctx.get(componentIDArgument);
+            player.sendMessage("Updating every component with ID " + id);
+
+            for(SidebarComponent<CytosisPlayer> component : player.getCurrentSidebar().getComponents()) {
+                if(id.equals(component.getId())) {
+                    player.updateComponent(component);
+                }
+            }
+
+        }, componentIDArgument);
+    }
+
+}
